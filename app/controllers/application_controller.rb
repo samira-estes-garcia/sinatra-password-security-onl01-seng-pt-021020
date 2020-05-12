0ea08d9e08cc3c8abd6b645cc32b5f8b29c1f67f
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-12 18:12:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,6 @@ class ApplicationController < Sinatra::Base
 		user = User.find_by(:username => params[:username])
  
     if user
-      session[:user_id] = user.id
       redirect "/success"
     else
       redirect "/failure"
